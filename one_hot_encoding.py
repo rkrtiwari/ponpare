7d79beba54708eb5141dfe219a9573ae48eb03c4
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2017-05-05 08:12:58 +0800

    changed variable names to adapt to current version

--- one_hot_encoding.py
+++ one_hot_encoding.py
@@ -24,12 +24,11 @@ from sklearn.preprocessing import Imputer
 ###############################################################################
 
 columns_to_use = ['PURCHASE_FLG', 'SEX_ID', 'AGE', 'GENRE_NAME', 'PRICE_RATE',
-                  'CATALOG_PRICE', 'VALIDFROM', 'VALIDEND']
+                  'CATALOG_PRICE']
 user_coupon_purchase_detail = visit_user_coupon_detail[columns_to_use]
+user_coupon_purchase_detail.dropna(axis = 0, how = 'any', inplace = True)
 
-###############################################################################
-# need to look into the code more carefully. worked for some other data now need 
-# to adapt for current data
+
 ###############################################################################
 # conversion into categorical variables
 ###############################################################################
@@ -39,30 +38,32 @@ X_cat = pd.DataFrame(columns = np.arange(5))
 bins = [0,20,30,40,50,60,100]
 sufs = np.arange(len(bins)-1)
 labels = ["age" + str(suf) for suf in sufs]
-X_cat[i] = pd.cut(user_purchase_subset_nona.AGE, bins = bins, labels = labels)
+X_cat[i] = pd.cut(user_coupon_purchase_detail.AGE, bins = bins, labels = labels)
 
 # 2. price rate
 i = i + 1
 bins = [0,25,50,60,70,80,90,100]
 sufs = np.arange(len(bins)-1)
 labels = ["price_rate" + str(suf) for suf in sufs]
-X_cat[i] = pd.cut(user_purchase_subset_nona.PRICE_RATE, bins = bins, labels = labels)
+X_cat[i] = pd.cut(user_coupon_purchase_detail.PRICE_RATE, bins = bins, labels = labels)
 
 # 3. catalog price
 i = i+1
 bins = [0, 1000, 2500, 5000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 1000000]
 sufs = np.arange(len(bins)-1)
 labels = ["catalog_price" + str(suf) for suf in sufs]
-X_cat[i] = pd.cut(user_purchase_subset_nona.CATALOG_PRICE, bins = bins, labels = labels)
+X_cat[i] = pd.cut(user_coupon_purchase_detail.CATALOG_PRICE, bins = bins, labels = labels)
 
 # 4. sex_id
 i = i+1
-X_cat[i] = user_purchase_subset_nona.SEX_ID
+X_cat[i] = user_coupon_purchase_detail.SEX_ID
 
 # 5. genre_name
 i = i+1
-X_cat[i] = user_purchase_subset_nona.GENRE_NAME
+X_cat[i] = user_coupon_purchase_detail.GENRE_NAME
 
+# 6. predicated variable 
+y = user_coupon_purchase_detail.PURCHASE_FLG
 
 # convert to numeric using one hot encoding
 X_encoded = pd.get_dummies(X_cat)
@@ -87,20 +88,3 @@ n = 1000
 actual_n = sum(y_test.values[ind[-1]:ind[-1]+n])
 actual_n*100/n
 
-#2. Random forest
-
-clf = RandomForestClassifier()
- 
-clf.fit(X_train, y_train)
-y_predict = clf.predict_proba(X_test)
-y_predict_class = clf.predict(X_test)
-
-
-ind = np.argsort(y_predict[:,1])
-y_predict[ind,1]
-
-n = 100
-actual_n = sum(y_test.values[ind[-1]:ind[-1]+n])
-actual_n*100/n
-
-sum(y_test == 1)*100/len(y_test)
